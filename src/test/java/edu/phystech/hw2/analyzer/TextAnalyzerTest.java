c54e0c2123d1eb8334db35a1964ca319ecaafb28
armerker <anbrey1342@mail.ru>
2026-04-09 20:41:14 +0300

    Update TextAnalyzerTest.java

--- src/test/java/edu/phystech/hw2/analyzer/TextAnalyzerTest.java
+++ src/test/java/edu/phystech/hw2/analyzer/TextAnalyzerTest.java
@@ -6,6 +6,74 @@ import java.util.List;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
+enum Label {
+    SPAM, NEGATIVE, TOO_LONG, OK
+}
+
+interface TextAnalyzer {
+    Label processText(String text);
+}
+
+abstract class KeywordAnalyzer implements TextAnalyzer {
+    protected abstract List<String> getKeywords();
+    protected abstract Label getLabel();
+    
+    @Override
+    public Label processText(String text) {
+        for (String keyword : getKeywords()) {
+            if (text.contains(keyword)) {
+                return getLabel();
+            }
+        }
+        return Label.OK;
+    }
+}
+
+class SpamAnalyzer extends KeywordAnalyzer {
+    private final List<String> keywords;
+    
+    public SpamAnalyzer(List<String> keywords) {
+        this.keywords = keywords;
+    }
+    
+    @Override
+    protected List<String> getKeywords() {
+        return keywords;
+    }
+    
+    @Override
+    protected Label getLabel() {
+        return Label.SPAM;
+    }
+}
+
+class NegativeTextAnalyzer extends KeywordAnalyzer {
+    private static final List<String> SAD_SMILEYS = List.of(":(", "=(", ":|");
+    
+    @Override
+    protected List<String> getKeywords() {
+        return SAD_SMILEYS;
+    }
+    
+    @Override
+    protected Label getLabel() {
+        return Label.NEGATIVE;
+    }
+}
+
+class TooLongTextAnalyzer implements TextAnalyzer {
+    private final int maxLength;
+    
+    public TooLongTextAnalyzer(int maxLength) {
+        this.maxLength = maxLength;
+    }
+    
+    @Override
+    public Label processText(String text) {
+        return text.length() > maxLength ? Label.TOO_LONG : Label.OK;
+    }
+}
+
 public class TextAnalyzerTest {
 
     @Test
