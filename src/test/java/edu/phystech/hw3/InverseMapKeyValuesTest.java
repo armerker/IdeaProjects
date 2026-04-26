969b9e68c449f94d59ecdd377ac6bf9ebce713a4
armerker <anbrey1342@mail.ru>
2026-04-27 01:43:37 +0300

    добавление импортов

--- src/test/java/edu/phystech/hw3/InverseMapKeyValuesTest.java
+++ src/test/java/edu/phystech/hw3/InverseMapKeyValuesTest.java
@@ -4,7 +4,8 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-
+import java.util.HashMap;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
