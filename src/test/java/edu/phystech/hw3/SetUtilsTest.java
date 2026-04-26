3eefc516c6bc027501d9f7da35d03cc9ec7af103
armerker <anbrey1342@mail.ru>
2026-04-27 01:26:59 +0300

    задание 2

--- src/test/java/edu/phystech/hw3/SetUtilsTest.java
+++ src/test/java/edu/phystech/hw3/SetUtilsTest.java
@@ -6,6 +6,36 @@ import java.util.Set;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+public class SetUtils {
+    
+    public static <T> Set<T> union(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.addAll(second);
+        return result;
+    }
+    
+    public static <T> Set<T> intersection(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.retainAll(second);
+        return result;
+    }
+    
+    public static <T> Set<T> difference(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.removeAll(second);
+        return result;
+    }
+    
+    public static <T> Set<T> symmetricDifference(Set<T> first, Set<T> second) {
+        Set<T> result = new HashSet<>(first);
+        result.addAll(second);
+        Set<T> intersection = new HashSet<>(first);
+        intersection.retainAll(second);
+        result.removeAll(intersection);
+        return result;
+    }
+}
+
 public class SetUtilsTest {
 
     @Test
